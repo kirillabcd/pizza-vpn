ab1ca1d58297ee7005d635da6d5b9e8f8a8f74da
kirillabcd <k.motsart@gmail.com>
2024-03-06 11:46:27 +0200

    fix: trying to fix router on page update

--- src/js/components/buttons/link-button.js
+++ src/js/components/buttons/link-button.js
@@ -1,14 +1,23 @@
 import { directRouter } from '../../functions/vanilla-router'
 
-export const createLinkButton = (text, route) => {
+export const createLinkButton = (text, linkText, route) => {
+    const linkButtonContainer = document.createElement('div')
+    linkButtonContainer.className = 'link-button-container'
+
+    const linkButtonText = document.createElement('div')
+    linkButtonText.textContent = text
+    linkButtonText.className = 'link-button-text'
+
     const linkButton = document.createElement('a')
+    linkButton.textContent = linkText
     linkButton.className = 'link-button'
-    linkButton.textContent = text
+
+    linkButtonContainer.append(linkButtonText, linkButton)
 
     linkButton.addEventListener('click', (event) => {
         event.preventDefault
         directRouter(route)
     })
 
-    return linkButton
+    return linkButtonContainer
 }

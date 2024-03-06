f441ce402163e0c5b2d849a15495200ac5b1e910
kirillabcd <k.motsart@gmail.com>
2024-03-06 12:02:32 +0200

    fix:trying to fix router on page reload

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,16 @@ import '../scss/style.scss'
 import { router } from './functions/vanilla-router'
 import { handleAuthStatus } from './functions/handle-auth-status'
 
+if (sessionStorage.getItem('is_reloaded')) {
+    router()
+}
+sessionStorage.setItem('is_reloaded', true)
+
 window.addEventListener('popstate', () => {
     router()
 })
 
 window.addEventListener('DOMContentLoaded', () => {
-    router()
-})
-
-window.addEventListener('beforeunload', () => {
     router()
 })
 

0cd62d8c47b3062115ec74d9323234d2d9088024
kirillabcd <k.motsart@gmail.com>
2024-06-12 16:06:22 +0300

    feat: add sifn in page

--- src/views/SignInView/SignInView.jsx
+++ src/views/SignInView/SignInView.jsx
@@ -1,5 +1,26 @@
+import SignContainer from '../../components/SignContainer/SignContainer'
+import SignTitle from '../../components/SignTitle/SignTitle'
+import SignForm from '../../components/SignForm/SignForm'
+import EmailInput from '../../components/EmailInput/EmailInput'
+import PasswordInput from '../../components/PasswordInput/PasswordInput'
+import PrimaryButton from '../../components/PrimaryButton/PrimaryButton'
+
+const SignInTitleText = 'Вход'
+const primaryButtonText = 'Войти'
+
 function SignInView() {
-    return <h1>Sign In page</h1>
+    return (
+        <>
+            <SignContainer>
+                <SignTitle titleText={SignInTitleText}></SignTitle>
+                <SignForm>
+                    <EmailInput></EmailInput>
+                    <PasswordInput></PasswordInput>
+                    <PrimaryButton buttonText={primaryButtonText}></PrimaryButton>
+                </SignForm>
+            </SignContainer>
+        </>
+    )
 }
 
 export default SignInView
